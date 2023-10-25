333201329dd3014b6bc4530fadfafe246457f2ed
umairahmed88 <umairahmed_88@ymail.com>
2023-10-25 11:49:39 +0500

    Created header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,10 +5,12 @@ import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Search from "./pages/Search";
 import Profile from "./pages/Profile";
+import Header from "./components/Header";
 
 export default function App() {
 	return (
 		<BrowserRouter>
+			<Header />
 			<Routes>
 				<Route path='/' element={<Home />} />
 				<Route path='/about' element={<About />} />
