6b52df7cd31ced1e20651df61337a5340140f93a
umairahmed88 <umairahmed_88@ymail.com>
2023-10-26 11:50:53 +0500

    completed sign up functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,5 @@
-import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import User from "../models/user.model.js";
 
 export const signup = async (req, res, next) => {
 	const { username, email, password } = req.body;
@@ -7,7 +7,7 @@ export const signup = async (req, res, next) => {
 	const newUser = new User({ username, email, password: hashedPassword });
 	try {
 		await newUser.save();
-		res.status(200).json("User created successfully!");
+		res.status(201).json("User created successfully!");
 	} catch (error) {
 		next(error);
 	}
