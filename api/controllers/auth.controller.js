f1c953deed72106fb3d06ffd6ca2e13cd4889aba
umairahmed88 <umairahmed_88@ymail.com>
2023-10-29 00:10:27 +0500

    Added sign out functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -70,3 +70,12 @@ export const google = async (req, res, next) => {
 		next(error);
 	}
 };
+
+export const signout = (req, res, next) => {
+	try {
+		res.clearCookie("access_token");
+		res.status(200).json("User has been signed out!");
+	} catch (error) {
+		next(error);
+	}
+};
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -10,6 +10,9 @@ import { app } from "../firebase";
 import {
 	deleteUserFailure,
 	deleteUserSuccess,
+	signOutUserFailure,
+	signOutUserStart,
+	signOutUserSuccess,
 	updateUserFailure,
 	updateUserStart,
 	updateUserSuccess,
@@ -106,6 +109,21 @@ export default function Profile() {
 		}
 	};
 
+	const handleSignOut = async () => {
+		try {
+			dispatch(signOutUserStart());
+			const res = await fetch("/api/auth/signout");
+			const data = await res.json();
+			if (data.success === false) {
+				dispatch(signOutUserFailure(data.message));
+				return;
+			}
+			dispatch(signOutUserSuccess(data));
+		} catch (error) {
+			dispatch(signOutUserFailure(data.message));
+		}
+	};
+
 	return (
 		<div className=' p-3 max-w-lg mx-auto'>
 			<h1 className=' text-3xl font-semibold text-center my-7'>Profile</h1>
@@ -176,7 +194,9 @@ export default function Profile() {
 				>
 					Delete Account
 				</span>
-				<span className=' text-red-700 cursor-pointer'>Sign Out</span>
+				<span onClick={handleSignOut} className=' text-red-700 cursor-pointer'>
+					Sign Out
+				</span>
 			</div>
 			<p className=' text-red-700 mt-5'>{error ? error : ""}</p>
 			<p className=' text-green-700'>
