b4dc28f9e0361507406b3efbb75efe7c19a7905c
umairahmed88 <umairahmed_88@ymail.com>
2023-10-28 23:57:52 +0500

    Added delete user functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -8,6 +8,8 @@ import {
 } from "firebase/storage";
 import { app } from "../firebase";
 import {
+	deleteUserFailure,
+	deleteUserSuccess,
 	updateUserFailure,
 	updateUserStart,
 	updateUserSuccess,
@@ -85,6 +87,25 @@ export default function Profile() {
 		}
 	};
 
+	const handleDeleteUser = async () => {
+		try {
+			const res = await fetch(`/api/user/delete/${currentUser._id}`, {
+				method: "DELETE",
+			});
+
+			const data = await res.json();
+
+			if (data.success === false) {
+				dispatch(deleteUserFailure(data.message));
+				return;
+			}
+
+			dispatch(deleteUserSuccess(data));
+		} catch (error) {
+			dispatch(deleteUserFailure(error.message));
+		}
+	};
+
 	return (
 		<div className=' p-3 max-w-lg mx-auto'>
 			<h1 className=' text-3xl font-semibold text-center my-7'>Profile</h1>
@@ -149,7 +170,12 @@ export default function Profile() {
 				</button>
 			</form>
 			<div className=' justify-between flex mt-5'>
-				<span className=' text-red-700 cursor-pointer'>Delete Account</span>
+				<span
+					onClick={handleDeleteUser}
+					className=' text-red-700 cursor-pointer'
+				>
+					Delete Account
+				</span>
 				<span className=' text-red-700 cursor-pointer'>Sign Out</span>
 			</div>
 			<p className=' text-red-700 mt-5'>{error ? error : ""}</p>
