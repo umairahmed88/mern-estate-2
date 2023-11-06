26bbb8b0031febab67078ba5f52220363526e89c
umairahmed88 <umairahmed_88@ymail.com>
2023-11-06 11:30:07 +0500

    Added contact landlord functionality to the listing page

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,6 +1,7 @@
 import express from "express";
 import {
 	deleteUser,
+	getUser,
 	getUserListings,
 	test,
 	updateUser,
@@ -13,5 +14,6 @@ router.get("/test", test);
 router.post("/update/:id", verifyUser, updateUser);
 router.delete("/delete/:id", verifyUser, deleteUser);
 router.get("/listings/:id", verifyUser, getUserListings);
+router.get("/:id", verifyUser, getUser);
 
 export default router;
--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -1,3 +1,53 @@
-export default function Contact() {
-	return <div>Contact</div>;
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+export default function Contact({ listing }) {
+	const [landlord, setLandlord] = useState(null);
+	const [message, setMessage] = useState("");
+
+	const handleChange = (e) => {
+		setMessage(e.target.value);
+	};
+
+	useEffect(() => {
+		const fetchLandlord = async () => {
+			try {
+				const res = await fetch(`/api/user/${listing.userRef}`);
+				const data = await res.json();
+				setLandlord(data);
+			} catch (error) {
+				console.log(error);
+			}
+		};
+		fetchLandlord();
+	}, [listing.userRef]);
+
+	return (
+		<>
+			{landlord && (
+				<div className=' flex flex-col gap-2'>
+					<p>
+						Contact <span className=' font-semibold'>{landlord.username}</span>{" "}
+						for
+						<span className=' font-semibold'>{listing.name.toLowerCase()}</span>
+					</p>
+					<textarea
+						className=' w-full border p-3 rounded-lg'
+						placeholder='Enter your name message...'
+						name='message'
+						id='message'
+						rows='2'
+						value={message}
+						onChange={handleChange}
+					></textarea>
+					<Link
+						to={`mailto:${landlord.email}?subject=Regarding ${listing.name}&body=${message}`}
+						className=' bg-slate-700 text-white text-center p-3 uppercase rounded-lg hover:opacity-95'
+					>
+						Send message
+					</Link>
+				</div>
+			)}
+		</>
+	);
 }
